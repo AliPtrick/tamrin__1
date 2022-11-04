5944ad2798078f53d15d036b078e0fad5bbfd6af
AliPtrick <Ali7116000@gmail.com>
2022-11-04 19:53:13 +0330

    jadval zarb ba for

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table border="6">
+
+    <?php
+
+    for ($a = 1; $a <= 5; $a++) {
+        echo "<tr>";
+        for ($b = 1; $b <= 5; $b++) {
+            echo "<td>";
+            echo $a * $b;
+            echo "</td>";
+
+        }
+        echo "</br>";
+        echo "</tr>";
+
+    }
+    ?>
+</table>
+</body>
+</html>
